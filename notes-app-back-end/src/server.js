28863cca46bb377403c354207b6dbca3c0cdf49c
heyoktaaa <ofebrianto65@gmail.com>
2026-01-21 22:49:16 +0700

    update default port to 5001

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -4,7 +4,7 @@ import routes from './routes.js';
 import process from 'process';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 const host = process.env.NODE_ENV !== 'production'? 'localhost' : '0.0.0.0';
 
 app.use(cors({
